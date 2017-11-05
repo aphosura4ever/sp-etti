c1dd4e8ada99688f6dfe4457526e0122fd72a665
Stefan Denis <32988399+aphosura4ever@users.noreply.github.com>
2017-11-05 23:34:21 +0200

    E1_Stefan_Denis_Andrei_Cristian

---
+++ E1_Stefan_Denis_Andrei_Cristian.m
@@ -0,0 +1,28 @@
+%E1. Exercitiu.  
+%Fie vectorii linie a=[0,0.1,0.2, ,2] si coloana b=[1...1]
+ %a) Ce lungime trebuie sa aiba b astfel ca sa aiba sens înmultirea 
+%(în sens matricial) a*b? Ini?ializa?i în MATLAB cei doi vectori si 
+%realizati înmultirea. Ce operatie se efectueaza? 
+%b) Realizati înmultirea b*a. 
+%c) Realizati înmultirea element cu element a celor doi vectori. 
+%Ce rezultat se obtine? 
+ 
+% a)
+a=0:0.1:2
+c=ones(1,21) %creez un vector linie cu 21 de 1
+b=c';%transpusul lui c devine b si astfel am obtinut vectorul coloana cu 21 
+%de 1 
+
+% b trebuie sa aiba o singura coloana cu 21 linii pentru ca a*b sa aiba sens
+a*b
+% b)
+
+b*a
+
+%c)
+
+a.*b
+
+%Inmultirea element cu element va conduce la o eroare. Pentru ca inmultirea
+%element cu element sa aiba sens trebuie sa se inmulteasca 2
+%vectori linie sau 2 vectori coloana
